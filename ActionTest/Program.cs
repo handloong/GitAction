c53ef28829602322ae65241cccd1056118644705
HandLoong <397706388@qq.com>
2021-04-13 22:14:10 +0800

    添加项目文件。

---
+++ ActionTest/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Flurl.Http;
+
+namespace ActionTest
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var url = $@"http://push.ijingniu.cn/send?key=7374a9ff1b8c4a04a3afb38cc96f1d35&head=gitAction&body={DateTime.Now:yyyyMMdd_HH:mm:ss}";
+
+            var res = await url.GetAsync();
+            Console.WriteLine(res.ResponseMessage);
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
